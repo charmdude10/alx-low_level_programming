4e254b3ad46470a84b2bbc3b54f9b3893f90e115
charmdude10 <abioyemichael4@gmail.com>
2023-06-18 14:41:47 -0700

    3print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower
+ * and theen in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
