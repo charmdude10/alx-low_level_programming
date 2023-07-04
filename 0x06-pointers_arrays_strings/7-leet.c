940b76634dc3c38d9c9df67c6000d2b41e339eb0
charmdude10 <abioyemichael4@gmail.com>
2023-07-04 01:44:17 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @s: input value
+ * Return: s value
+ */
+char *leet(char *s)
+{
+int a, b;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; b < 10; b++)
+{
+if (s[a] == s1[b])
+{
+s[a] = s2[b];
+}
+}
+}
+return (s);
+}
