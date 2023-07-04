1aa409fb1927bb37f4687d6e7217f87846620c0e
charmdude10 <abioyemichael4@gmail.com>
2023-07-04 00:50:04 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,10 @@
 #include "main.h"
-#include <stdio.h>  
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
+ * @n: The pointer to an int.
  */
-int main(void)
+void reset_to_98(int *n)
 {
-char s1[98] = "Hello ";
-char s2[] = "World!\n";
-char *ptr;
-printf("%s\n", s1);
-printf("%s", s2);
-ptr = _strcat(s1, s2);
-printf("%s", s1);
-printf("%s", s2);
-printf("%s", ptr);
-return (0);
+*n = 98;
 }
