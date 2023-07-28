bfdd67d1188ac28ead223eb3a8408316786225ec
charmdude10 <abioyemichael4@gmail.com>
2023-07-27 18:58:28 -0700

    linked

---
+++ 0x12-singly_linked_list/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Character to be printed
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropraitely.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
